76f774c671cca9e170c8efe95fa71ba508d2bb81
irines <irina.kiselyowa12@gmail.com>
2023-06-08 16:10:19 +0300

    custom filter func., bubbleSort, closure function

--- index.js
+++ index.js
@@ -1,125 +1,75 @@
-function makeDeepCopy(obj) {
-    if (typeof obj !== "object" || obj == null || obj instanceof Map || obj instanceof Set || Array.isArray(obj)) {
-        throw new Error();
-    } else {
-        let copy = recursiveCopy(obj);
-        return copy;
+const myCustomFilter = function (callback, contextObj = null) {
+    if (!callback || typeof callback !== "function") {
+        throw new Error("Invalid argument.");
     }
-}
-
-function getStringType(value) {
-    let _toString = Object.prototype.toString;
-    let str = _toString.call(value);
-    return str.slice(8, -1);
-}
-
-// WeakMap is used to keep track of already copied objects and prevent looping when copying objects with circular references.
-function recursiveCopy(target, map = new WeakMap()) {
-    // clone primitive types
-    if (typeof target != "object" || target == null) {
-        return target;
+    if (contextObj && typeof contextObj !== "object") {
+        throw new Error("Invalid argument.");
     }
+    let filteredArr = [];
 
-    const type = getStringType(target);
-    let cloneTarget = null;
-
-    if (map.get(target)) {
-        return map.get(target);
-    }
-    map.set(target, cloneTarget);
-
-    // in case if String/Number/Boolean created with function-constructor
-    if (type != "Set" && type != "Map" && type != "Array" && type != "Object") {
-        return target;
-    }
-
-    // clone Set
-    if (type == "Set") {
-        cloneTarget = new Set();
-        target.forEach((value) => {
-            cloneTarget.add(recursiveCopy(value, map));
-        });
-        return cloneTarget;
-    }
-
-    // clone Map
-    if (type == "Map") {
-        cloneTarget = new Map();
-        target.forEach((value, key) => {
-            cloneTarget.set(key, recursiveCopy(value, map));
-        });
-        return cloneTarget;
-    }
-
-    // clone Array
-    if (type == "Array") {
-        cloneTarget = new Array();
-        for (const key in target) {
-            cloneTarget[key] = recursiveCopy(target[key], map);
-        }
-        return cloneTarget;
-    }
-
-    // clone Object
-    if (type == "Object") {
-        cloneTarget = new Object();
-        for (const key in target) {
-            cloneTarget[key] = recursiveCopy(target[key], map);
-        }
-        return cloneTarget;
-    }
-
-    return cloneTarget;
-}
-
-function createIterable(from, to) {
-    let iterable = {
-        from: from,
-        to: to,
-        [Symbol.iterator]: function () {
-            return {
-                current: this.from,
-                last: this.to,
-                next() {
-                    if (this.current <= this.last) {
-                        return { done: false, value: this.current++ };
-                    } else {
-                        return { done: true };
-                    }
-                },
-            };
-        },
-    };
-
-    return iterable;
-}
-
-function createProxy(obj) {
-    if (!obj || typeof obj !== "object" || Array.isArray(obj)) {
-        throw new Error();
-    }
-
-    return new Proxy(obj, {
-        get(target, prop) {
-            if (prop in target) {
-                target[prop].readAmount = (target[prop].readAmount || 0) + 1;
-                return target[prop].value;
+    for (let i = 0; i < this.length; i++) {
+        if (contextObj === null) {
+            if (callback(this[i], i, this)) {
+                filteredArr.push(this[i]);
             }
-            return undefined;
-        },
-        set(target, prop, value) {
-            if (prop in target) {
-                const existingValue = target[prop].value;
-                if (typeof existingValue === typeof value) {
-                    target[prop].value = value;
-                } else {
-                    return false;
-                }
-            } else if (typeof prop === "string" || typeof prop === "number" || typeof prop === "symbol") {
-                target[prop] = { value, readAmount: 0 };
-            } else return false;
+        } else {
+            if (callback.call(contextObj, this[i], i, this)) {
+                filteredArr.push(this[i]);
+            }
+        }
+    }
+    return filteredArr;
+};
 
-            return true;
-        },
-    });
-}
+Array.prototype.customFilter = myCustomFilter;
+
+const bubbleSort = (input) => {
+    validateArr(input);
+    let arr = [...input];
+    for (let i = 0; i < arr.length - 1; i++) {
+        let swapDuringCycle = false;
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let save = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = save;
+                swapDuringCycle = true;
+            }
+        }
+        if (!swapDuringCycle) break;
+    }
+    return arr;
+};
+
+const validateArr = (arr) => {
+    if (!Array.isArray(arr)) {
+        throw new Error("Invalid argument.");
+    }
+    for (let el of arr) {
+        if (typeof el !== "number" || isNaN(el) || !isFinite(el)) {
+            throw new Error("Invalid argument.");
+        }
+    }
+};
+
+const storageWrapper = (callback, arr = null) => {
+    if (!callback || typeof callback !== "function") {
+        throw new Error("Invalid argument.");
+    }
+    if (arr && !Array.isArray(arr)) {
+        throw new Error("Invalid argument.");
+    }
+
+    let closuredArr = [];
+    if (!arr) {
+        return () => {
+            return (closuredArr = [...closuredArr, callback()]);
+        };
+    } else {
+        return () => {
+            const newVal = callback();
+            arr.push(newVal);
+            return newVal;
+        };
+    }
+};
